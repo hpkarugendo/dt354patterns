4068dc7c6ff806b5ca58ac1542b37db1d01f90d6
HPKarugendo <c12716891@mydit.ie>
2016-04-15 11:49:39 +0100

    Final changes

--- dt354patterns/src/main/java/ie/dit/dt354patters/controllers/BookOrderController.java
+++ dt354patterns/src/main/java/ie/dit/dt354patters/controllers/BookOrderController.java
@@ -3,10 +3,12 @@ package ie.dit.dt354patters.controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.number.money.MonetaryAmountFormatter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.stormpath.sdk.account.Account;
 import com.stormpath.sdk.servlet.account.AccountResolver;
-
-import ie.dit.dt354patters.cheats.Money;
 import ie.dit.dt354patters.model.Book;
 import ie.dit.dt354patters.model.BookItem;
 import ie.dit.dt354patters.model.BookOrder;
@@ -37,57 +37,58 @@ public class BookOrderController {
     @RequestMapping("order/add/{id}")
     public String addtoCart(Model model, HttpServletRequest req, @PathVariable Integer id){
 		Account acc = AccountResolver.INSTANCE.getAccount(req);
-		Customer cus = cRepo.findByEmail(acc.getEmail());
-		if(orders.isEmpty()){
-			BookOrder b = boRepo.findByCustomerAndNewOrder(cus, true);
-			if(b == null){
-				b = new BookOrder();
-				b.setCustomer(cus);
-				b.setNewOrder(true);
-				BookOrder bo2 = boRepo.save(b);
-				orders.put(cus.getEmail(), bo2);
-			}
-		}
-		
-		BookOrder bo = orders.get(cus.getEmail());
-		Book bk = bRepo.findOne(id);
-		boolean itemFound = false;
-		if(bo == null || bo.getBookItems().size() < 1){
-			bo = new BookOrder();
-			bo.setCustomer(cus);
-			bo.setNewOrder(true);
-			List<BookItem> items = new ArrayList<BookItem>();
-			BookItem bi = new BookItem();
-			bi.setBook(bk);
-			bi.setQuantity(1);
-			items.add(bi);
-			bo.setBookItems(items);
-			boRepo.save(bo);
+		if(acc == null) {
+			return "redirect:/login";
 		} else {
-			for(BookItem i : bo.getBookItems()){
-				if(bk.getId() == i.getBook().getId()){
-					i.setQuantity(i.getQuantity() + 1);
-					boRepo.save(bo);
-					itemFound = true;
+			Customer cus = cRepo.findByEmail(acc.getEmail());
+			if(orders.isEmpty()){
+				BookOrder b = boRepo.findByCustomerAndNewOrder(cus, true);
+				if(b == null){
+					b = new BookOrder();
+					b.setCustomer(cus);
+					b.setNewOrder(true);
+					BookOrder bo2 = boRepo.save(b);
+					orders.put(cus.getEmail(), bo2);
 				}
 			}
 			
-			if(itemFound == false){
-				
-			} else {
+			BookOrder bo = orders.get(cus.getEmail());
+			Book bk = bRepo.findOne(id);
+			boolean itemFound = false;
+			if(bo == null || bo.getBookItems().size() < 1){
+				bo = new BookOrder();
+				bo.setCustomer(cus);
+				bo.setNewOrder(true);
+				List<BookItem> items = new ArrayList<BookItem>();
 				BookItem bi = new BookItem();
 				bi.setBook(bk);
 				bi.setQuantity(1);
-				bo.getBookItems().add(bi);
+				items.add(bi);
+				bo.setBookItems(items);
 				boRepo.save(bo);
+			} else {
+				for(BookItem i : bo.getBookItems()){
+					if(bk.getId() == i.getBook().getId()){
+						i.setQuantity(i.getQuantity() + 1);
+						boRepo.save(bo);
+						itemFound = true;
+					}
+				}
+				
+				if(itemFound == false){
+					
+				} else {
+					BookItem bi = new BookItem();
+					bi.setBook(bk);
+					bi.setQuantity(1);
+					bo.getBookItems().add(bi);
+					boRepo.save(bo);
+				}
 			}
+			model.addAttribute("order", bo);
+			model.addAttribute("total", bo.getOrderTotal());
+			
+			return "cart";
 		}
-		
-		Money m = new Money();
-		String total = m.turn(bo.getOrderTotal());
-		model.addAttribute("order", bo);
-		model.addAttribute("total", total);
-		
-		return "cart";
     }
 }
